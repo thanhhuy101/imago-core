240953c99bb483bc41ba921a036fb5a6fae64b94
Honekawa <manhvp1810@gmail.com>
2024-02-25 23:03:12 +0700

    tạo sẵn các file và folder cho các domain đã được hoạch định

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './client/auth/auth.module';
+import { AuthModule } from './internal/auth/auth.module';
+import { CategoryModule } from './internal/category/category.module';
+import { CommentModule } from './internal/comment/comment.module';
+import { PostModule } from './internal/post/post.module';
+import { ProfileModule } from './internal/profile/profile.module';
+import { RoleModule } from './internal/role/role.module';
+import { StorageModule } from './internal/storage/storage.module';
+import { ReportModule } from './internal/report/report.module';
+import { HashtagModule } from './internal/hashtag/hashtag.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CategoryModule, CommentModule, PostModule, ProfileModule, RoleModule, StorageModule, ReportModule, HashtagModule],
   controllers: [AppController],
   providers: [AppService],
 })
