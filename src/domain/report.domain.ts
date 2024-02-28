1866a4ba57dc39289bbcf57399c05650da3e545f
Vo Minh Tri <vominhtri1610@gmail.com>
2024-02-28 16:31:14 +0700

    report

--- src/domain/report.domain.ts
+++ src/domain/report.domain.ts
@@ -6,3 +6,23 @@ export interface Report {
   typeId: string;
   content: string;
 }
+
+export interface ReportRepository {
+  create(report: Report): Promise<Report>;
+  update(id: string): Promise<Report>;
+  getAll(): Promise<Report[]>;
+}
+
+export interface ReportUseCase {
+  create(report: Report): Promise<Report>;
+  update(id: string): Promise<Report>;
+  getAll(): Promise<Report[]>;
+}
+
+export interface ReportInterop {
+  create(report: Report): Promise<Report>;
+  update(id: string, token: string): Promise<Report>;
+  getAll(token: string): Promise<Report[]>;
+}
+
+
