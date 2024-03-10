38ac9e10d5534750917f417bc2048705662bafca
bqtruong2000 <bqtruong2021@gmail.com>
2024-03-10 19:17:56 +0700

    create notification

---
+++ src/internal/notification/delivery/notification.controller.ts
@@ -0,0 +1,59 @@
+import { Body, Controller, Inject, Post, Headers, Query } from '@nestjs/common';
+import { NotificationDomain, NotificationInterop } from '../../../domain/notification.domain';
+
+@Controller('v1/notification')
+export class NotificationController {
+  constructor(@Inject('NotificationInterop') private interop: NotificationInterop){
+
+  }
+
+  @Post()
+  async createNotification(@Headers() headers:any, @Body() notification: NotificationDomain){
+    let token = headers['authorization'];
+    try {
+      return await this.interop.create(token, notification);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @Post('all')
+  async getNotifications(@Headers() headers:any, @Query('uid') uid: string){
+    let token = headers['authorization'];
+    try {
+      return await this.interop.getNotificationsByUid(uid, token);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @Post('follow')
+  async getFollowNotifications(@Headers() headers:any, @Query('uid') uid: string){
+    let token = headers['authorization'];
+    try {
+      return await this.interop.getNotificationsByFollow(uid, token);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @Post('like')
+  async getLikeNotifications(@Headers() headers:any, @Query('uid') uid: string){
+    let token = headers['authorization'];
+    try {
+      return await this.interop.getNotificationsByLike(uid, token);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @Post('comment')
+  async getCommentNotifications(@Headers() headers:any, @Query('uid') uid: string){
+    let token = headers['authorization'];
+    try {
+      return await this.interop.getNotificationsByComment(uid, token);
+    } catch (e) {
+      throw e;
+    }
+  }
+}
